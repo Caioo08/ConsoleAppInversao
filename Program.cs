2cc41da8099b509d3b3915112496508b6566e729
Usuario <Usuario@CPD02-M12>
2024-02-27 11:59:37 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppInversao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o valor de A: ");
+            string a = Console.ReadLine();
+
+            Console.Write("Digite o valor de B: ");
+            string b = Console.ReadLine();
+
+            string b1 = a;
+
+            a = b;
+
+            b = b1;
+
+            Console.WriteLine("O valor de B: " + a);
+            Console.Write(" Valor de A: " + b);
+
+            Console.ReadKey();
+
+        }
+    }
+}
